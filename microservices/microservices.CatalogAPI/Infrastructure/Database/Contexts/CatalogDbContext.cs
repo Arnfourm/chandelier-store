18996a6c3ca3b6db4d4128b981783795937e503e
luver <arnfourm@gmail.com>
2025-10-27 23:45:37 +0300

    Catalog microservice started. Added file structure

---
+++ microservices/microservices.CatalogAPI/Infrastructure/Database/Contexts/CatalogDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace microservices.CatalogAPI.Infrastructure.Database.Contexts
+{
+    public class CatalogDbContext : DbContext
+    {
+        public CatalogDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        protected CatalogDbContext()
+        {
+        }
+
+
+    }
+}
---
+++ microservices/microservices.CatalogAPI/Infrastructure/Database/Entities/ProductEntity.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace microservices.CatalogAPI.Infrastructure.Database.Entities
+{
+    [Table("Product")]
+    public class ProductEntity
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public Guid Id { get; set; }
+
+        [Required]
+        [StringLength(75)]
+        public required string Article { get; set; }
+
+        [Required]
+        [StringLength(200)]
+        public required string Name { get; set; }
+
+        [Required]
+        public float Price { get; set; }
+    }
+}
