f61c2ad391c4ee13e9d16ed58f8900a0d0e0d1c7
Arnfourm <arnfourm@gmail.com>
2025-10-28 13:43:26 +0300

    Added some entities database

---
+++ microservices/microservices.CatalogAPI/Infrastructure/Database/Entities/ColorEntity.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace microservices.CatalogAPI.Infrastructure.Database.Entities
+{
+    [Table("Color")]
+    public class ColorEntity
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        public required string Title { get; set; }
+    }
+}
---
+++ microservices/microservices.CatalogAPI/Infrastructure/Database/Entities/LightingDeviceTypeEntity.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace microservices.CatalogAPI.Infrastructure.Database.Entities
+{
+    [Table("LightingDeviceType")]
+    public class LightingDeviceTypeEntity
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(150)]
+        public required string Title { get; set; }
+    }
+}
