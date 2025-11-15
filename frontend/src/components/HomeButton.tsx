ae7d1ccd05afe1d8e30dfb7356a0903ff5348e19
Vika Manaicheva <manaichevavika@gmail.com>
2025-11-16 01:27:23 +0300

    feat: Home page layout done

--- frontend/src/components/HomeButton.tsx
+++ frontend/src/components/HomeButton.tsx
@@ -6,8 +6,8 @@ export function HomeButton({ name, ref }) {
             <Link to={ref} className="font-semibold text-xl mr-[10px]">
                 {name}
             </Link>
-            <div className="w-[28px] h-[28px] bg-amber-600 flex items-center justify-center rounded-full">
-                <span className="text-2xl text-neutral-100 font-bold">&#8250;</span>
+            <div className="w-[28px] h-[28px] bg-amber-500 flex items-center justify-center rounded-full">
+                <span className="text-2xl text-neutral-50 font-bold">&#8250;</span>
             </div>
         </div>
     );
--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,24 +1,14 @@
-import { Link } from "react-router-dom";
-import { HomeButton } from "../components/HomeButton";
 import { Footer } from "../modules/Footer";
 import { Navbar } from "../modules/Navbar";
+import { Promo } from "../modules/Promo";
+import { Slider } from "../modules/Slider";
 
 export function Home() {
     return (
         <>
             <Navbar />
-            <div className="h-[600px] bg-neutral-200">
-                <h1>Это страница "Главная"</h1>
-                <HomeButton name={"Перейти в каталог"} ref={"/catalog"} />
-            </div>
-            <div className="text-center flex flex-col items-center mt-[3%] mb-[3%]">
-                <h2 className="uppercase font-bold text-5xl w-[74%] mb-[2%]">
-                    <span className="text-amber-500">Империя люстр</span> – российская компания по
-                    продаже светодиодного оборудования, эксперт в области интерьерного и уличного
-                    освещения
-                </h2>
-                <HomeButton name={"О компании"} ref={"/contacts"} />
-            </div>
+            <Slider />
+            <Promo />
             <Footer />
         </>
     );
