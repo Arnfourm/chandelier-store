1658baa699c581c8ba7de815647800c6addd68ba
luver <arnfourm@gmail.com>
2025-11-25 17:45:52 +0300

    Resolve conflict of automate script

--- automate/postgres-creation.old.sh
+++ automate/postgres-creation.old.sh
@@ -9,7 +9,7 @@ docker pull postgres:15-alpine3.21
 docker run --name chandelier-store-db -e POSTGRES_USER="luver" -e POSTGRES_PASSWORD="987654321" -d -p 5432:5432  postgres:15-alpine3.21
 
 # Db migrations to update db
-cd ./microservices/microservices.CatalogAPI/
+# cd ./microservices/microservices.CatalogAPI/
 
 # ! Приложение должно быть выключено
 # ! Команды вводить в powershell (от администратора)
